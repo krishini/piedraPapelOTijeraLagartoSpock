23a45a19b92a693afccf76d90818866069e4ce66
lilulil <lucilabelenp@gmail.com>
2021-07-10 10:56:58 -0300

    commit1

---
+++ src/piedrapapelotijeralagartospock/Piedrapapelotijeralagartospock.java
@@ -0,0 +1,33 @@
+package piedrapapelotijeralagartospock;
+
+class Piedrapapelotijeralagartospock {
+
+	public static void main(String[] args) {
+	int piedra = 0, papel = 1, tijera = 2, lagarto = 3, spock = 4, jugadaMaquina;
+	String jugadaHumano;
+	
+	JOptionPane.showMessageDialog(null, "Mi mensaje","Mi mensaje parte 2",
+			JOptionPane.DEFAULT_OPTION,new ImageIcon(nomredemiclase.class.getResource("/img/piedra-papel-tijera-lagarto-spock.jpeg")));
+
+	
+/*
+	al mejor de 5 jugadas , al finalizar indicar quien es el ganador
+	Pueden agregar apuesta en dólares
+	Al finalizar preguntar si quiere volver a jugar si/no.
+	
+
+Tijera corta a papel
+papel envuelve a piedra
+piedra aplasta a lagarto
+lagarto envenena a Spock
+Spock rompe a tijera
+tijera decapita a lagarto
+lagarto devora a papel
+papel desautoriza a Spock
+Spock desintegra a piedra
+piedra aplasta a tijera".
+  
+ */
+	}
+
+}
